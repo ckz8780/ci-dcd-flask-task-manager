adf90c00413e7ad64a887d7509c220bed89adef8
Chris Zielinski <ckz8780@gmail.com>
2018-06-01 19:37:42 +0000

    Added submit button and form handling

--- app.py
+++ app.py
@@ -21,7 +21,13 @@ def get_tasks():
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+    
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks =  mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
