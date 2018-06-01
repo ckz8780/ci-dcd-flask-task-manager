543ccb59e0abca8f873b603289b5770160b886e8
Chris Zielinski <ckz8780@gmail.com>
2018-06-01 18:23:29 +0000

    Added tasks template and basic DB connection functionality

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+MONGO_USER = os.environ.get('MONGO_USER')
+MONGO_PASS = os.environ.get('MONGO_PASS')
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://{}:{}@ds245150.mlab.com:45150/{}'.format(MONGO_USER, MONGO_PASS, app.config['MONGO_DBNAME'])
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World ... again!'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", 
+    tasks=mongo.db.tasks.find())
     
 if __name__ == '__main__':
-    app.run(
-        host = os.environ.get('IP'),
-        port = int(os.environ.get('PORT')),
-        debug = True
-    )
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
+    
