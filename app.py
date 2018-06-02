a805c8d1a17d043427e2bd01f697c0fea8c83245
Chris Zielinski <ckz8780@gmail.com>
2018-06-02 18:41:35 +0000

    Added nav menu and final styling

--- app.py
+++ app.py
@@ -38,16 +38,14 @@ def edit_task(task_id):
 @app.route('/update_task/<task_id>', methods=['POST'])
 def update_task(task_id):
     tasks = mongo.db.tasks
-    tasks.update(
-        {'_id': ObjectId(task_id)},
-        {
-            'task_name' : request.form.get['task_name'],
-            'category_name' : request.form.get['category_name'],
-            'task_description' : request.form.get['task_description'],
-            'due_date' : request.form.get['due_date'],
-            'is_urgent' : request.form.get['is_urgent']
-        }
-    )
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name' : request.form.get('task_name'),
+        'category_name' : request.form.get('category_name'),
+        'task_description' : request.form.get('task_description'),
+        'due_date' : request.form.get('due_date'),
+        'is_urgent' : request.form.get('is_urgent')
+    })
     return redirect(url_for('get_tasks'))
     
 @app.route('/delete_task/<task_id>')
